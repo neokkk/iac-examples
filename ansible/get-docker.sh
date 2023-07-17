14825186fcf30d972adc62fe746985f51a26f452
neokkk <seven3126@gmail.com>
2023-07-18 01:53:23 +0900

    feat: docker registry

---
+++ ansible/get-docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USER=vagrant
+HOME=/home/$USER
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg -y
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(lsb_release -cs)" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+mkdir $HOME/.docker
+
+sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+sudo chmod g+rwx "$HOME/.docker" -R
+
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
